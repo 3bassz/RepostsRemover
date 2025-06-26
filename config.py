c5024c3b1c97b004770b333db3e9b9f01854b8fa
3bassz <ahmedmabbas46@gmail.com>
2025-06-26 04:39:25 +0300

    Initial commit for Telegram Repost Remover Bot

--- config.py
+++ config.py
@@ -1,2 +1,7 @@
-BOT_TOKEN = "7356689752:AAHm7brZirQvlh9ilxnfrvaQmW26kb-Bv-8"
-OWNER_CHAT_ID = 6949215167  # ضع هنا رقمك كمالك البوت
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("7356689752:AAHm7brZirQvlh9ilxnfrvaQmW26kb-Bv-8")
+OWNER_CHAT_ID = int(os.getenv("6949215167"))
